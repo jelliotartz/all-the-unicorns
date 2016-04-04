af78762e568b44c049f3467a93f1f94cab5809ff
jelliotartz <jelliotartz@gmail.com>
2016-04-03 21:02:27 -0500

    create giphy API call

---
+++ allTheUnicorns/app/pages/unicorns/unicorn-details/unicorn-details.ts
@@ -0,0 +1,14 @@
+import {Page, NavController, NavParams} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/unicorns/unicorn-details/unicorn-details.html'
+})
+
+export class UnicornDetailsPage {
+	selectedUnicorn: any;
+
+	constructor(private nav: NavController, navParams: NavParams) {
+
+  }
+}
